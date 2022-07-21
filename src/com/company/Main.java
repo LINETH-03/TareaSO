d6b6357642a9a4c644e67302f40ad3566a2a6550
LINETH-03 <lperezh9@miumg.edu.gt>
2022-07-21 15:48:47 -0600

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("cara o cruz");
+
+
+
+
     }
+    public  void aleatorio(){
+        Random numAleatorio = new Random();
+
+// Genera un boolean de forma aleatoria
+        boolean moneda = numAleatorio.nextBoolean();
+
+// Si es verdadero cara y sino cruz
+        if (moneda) {
+            System.out.println("Cara");
+        } else {
+            System.out.println("Cruz");
+        }
+
+
+
+    }
+
+
 }
